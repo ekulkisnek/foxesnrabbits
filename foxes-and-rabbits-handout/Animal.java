f27bbbb61b61f1202e77c7883e64b140ded74daf
ekulkisnek <37669784+ekulkisnek@users.noreply.github.com>
2019-02-07 12:51:05 +0000

    Add files via upload

---
+++ foxes-and-rabbits-handout/Animal.java
@@ -0,0 +1,139 @@
+import java.util.List;
+import java.util.Random;
+
+/**
+ * A class representing shared characteristics of animals.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29 (2)
+ */
+public abstract class Animal
+{
+    // Whether the animal is alive or not.
+    private boolean alive;
+    // The animal's field.
+    private Field field;
+    // The animal's position in the field.
+    private Location location;
+    
+    private static final int BREEDING_AGE = 15;
+    // The age to which a fox can live.
+    private static final int MAX_AGE = 150;
+    // The likelihood of a fox breeding.
+    private static final double BREEDING_PROBABILITY = 0.08;
+    // The maximum number of births.
+    private static final int MAX_LITTER_SIZE = 2;
+    // The food value of a single rabbit. In effect, this is the
+    // number of steps a fox can go before it has to eat again.
+    private static final int RABBIT_FOOD_VALUE = 9;
+    // A shared random number generator to control breeding.
+    private static final Random rand = Randomizer.getRandom();
+    
+    // Individual characteristics (instance fields).
+    // The fox's age.
+    private int age;
+    // The fox's food level, which is increased by eating rabbits.
+    private int foodLevel;
+    
+    // Rabbit's sex, false is Female.
+    private boolean isMale;
+    
+    /**
+     * Create a new animal at location in field.
+     * 
+     * @param field The field currently occupied.
+     * @param location The location within the field.
+     */
+    public Animal(boolean randomAge, Field field, Location location)
+    {
+        alive = true;
+        this.field = field;
+        setLocation(location);
+        setSex();
+        
+        age = 0;
+        if(randomAge) {
+            age = rand.nextInt(MAX_AGE);
+        }
+    }
+    
+    /**
+     * Make this animal act - that is: make it do
+     * whatever it wants/needs to do.
+     * @param newAnimals A list to receive newly born animals.
+     */
+    abstract public void act(List<Animal> newAnimals);
+
+    /**
+     * Check whether the animal is alive or not.
+     * @return true if the animal is still alive.
+     */
+    protected boolean isAlive()
+    {
+        return alive;
+    }
+    
+    protected boolean isMale()
+    {
+        return isMale;
+    }
+    
+    /**
+     * Indicate that the animal is no longer alive.
+     * It is removed from the field.
+     */
+    protected void setDead()
+    {
+        alive = false;
+        if(location != null) {
+            field.clear(location);
+            location = null;
+            field = null;
+        }
+    }
+    
+    
+    protected void setSex() 
+    {
+        int sexNum = rand.nextInt(1000);
+        if(sexNum < 500) 
+        {
+            isMale = true;
+        }
+        else
+        {
+            isMale = false;
+        }
+    }
+
+    /**
+     * Return the animal's location.
+     * @return The animal's location.
+     */
+    protected Location getLocation()
+    {
+        return location;
+    }
+    
+    /**
+     * Place the animal at the new location in the given field.
+     * @param newLocation The animal's new location.
+     */
+    protected void setLocation(Location newLocation)
+    {
+        if(location != null) {
+            field.clear(location);
+        }
+        location = newLocation;
+        field.place(this, newLocation);
+    }
+    
+    /**
+     * Return the animal's field.
+     * @return The animal's field.
+     */
+    protected Field getField()
+    {
+        return field;
+    }
+}
